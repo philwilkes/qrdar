34ead8a196e85498e555caaee39d5a43e96cd49d
philwilkes <p.wilkes@ucl.ac.uk>
2019-05-09 15:16:39 +0100

    filtering steps and more logical

--- qrdar/locateTargets.py
+++ qrdar/locateTargets.py
@@ -33,27 +33,51 @@ def locateTargets(pc, markerTemplate=None, min_intensity=0, rmse_threshold=.15, 
     # target_centre = pc.loc[pc.labels_.isin(potential_dots.index)].groupby('labels_').mean()
     dbscan = DBSCAN(eps=.4, min_samples=3).fit(potential_dots[['x', 'y', 'z']])
     potential_dots.loc[:, 'target_labels_'] = dbscan.labels_
-    potential_dots = potential_dots[potential_dots.target_labels_ != -1] 
+    potential_dots = potential_dots[potential_dots.target_labels_ != -1]
+    print 'number of potential targets:', len(potential_dots.target_labels_.unique())
     
     # check if targets are too close
-    while np.any(potential_dots.target_labels_.value_counts().values > 4):
+    while np.any(~potential_dots.target_labels_.value_counts().isin([3, 4])):
+        
         for labels in potential_dots.target_labels_.unique():
+            
+            remove = False
             bright_spots_tmp = potential_dots[potential_dots.target_labels_ == labels]
+            print 'processing: {} (number of stickers {})'.format(labels, len(bright_spots_tmp))
+            
+            # remove stickers that don't match ~ distance between stickers
+            remove_idx = distanceFilter(bright_spots_tmp, markerTemplate)
+            potential_dots = potential_dots.loc[~potential_dots.index.isin(remove_idx)]
+            bright_spots_tmp = bright_spots_tmp.loc[~bright_spots_tmp.index.isin(remove_idx)]
+            if len(remove_idx) > 0:
+                print "\tstickers removed for wrong distance:", len(remove_idx)
+            
+            if len(bright_spots_tmp) < 3:
+                reason = 'less than 3'
+                remove = True
+            
             if len(bright_spots_tmp) > 4:
-                if verbose: print 'filtering points matching label {} with {} points'.format(labels, len(bright_spots_tmp))
                 idx, R, rmse = calculate_R(bright_spots_tmp, markerTemplate)
-                potential_dots.loc[idx, 'target_labels_'] = potential_dots.target_labels_.max() + 1
-    
-    # remove any stickers that are not associated wth at least 2 others
-    vc = potential_dots.target_labels_.value_counts()
-    potential_dots = potential_dots[potential_dots.target_labels_.isin(vc[vc >= 3].index)]
-    
-    # and double check (remove any with large error that have been left over)
-    for labels in potential_dots.target_labels_.unique():
-        bright_spots_tmp = potential_dots[potential_dots.target_labels_ == labels]
-        idx, R, rmse = calculate_R(bright_spots_tmp, markerTemplate)
-        if rmse > .02 and .1 < bright_spots_tmp.z.ptp() < .4:
-            potential_dots = potential_dots.loc[potential_dots.target_labels_ != labels]
+                if np.isnan(rmse):
+                    reason = 'does not fit template'
+                    remove = True
+                else:
+                    labels = potential_dots.target_labels_.max() + 1
+                    bright_spots_tmp = bright_spots_tmp.loc[idx]
+                    potential_dots.loc[idx, 'target_labels_'] = labels
+            
+            if 3 <= len(bright_spots_tmp) <= 4:
+                if bright_spots_tmp.z.ptp() < .1 or bright_spots_tmp.z.ptp() > .4:
+                    reason = 'points are not spread over Z correctly' 
+                    remove = True
+                idx, R, rmse = calculate_R(bright_spots_tmp, markerTemplate)
+                if np.isnan(rmse):
+                    reason = 'rmse greater than threshold'
+                    remove = True
+
+            if remove:
+                if verbose: print "\tremvoing targets labelled: {} {}".format(labels, reason)
+                potential_dots = potential_dots[potential_dots.target_labels_ != labels]
     
     # find code centres
     code_centres = potential_dots.groupby('target_labels_')[['x', 'y', 'z']].mean().reset_index()
