c46fb2f7f78b6d171f90d41605b5ae1d5dbe8f40
philwilkes <p.wilkes@ucl.ac.uk>
2020-06-30 12:25:01 +0100

    python 3 print statements and min_size option in filterBySize

--- qrdar/search4stickers.py
+++ qrdar/search4stickers.py
@@ -6,6 +6,8 @@ from sklearn.cluster import DBSCAN
 from qrdar.io.pcd_io import *
 from qrdar.io.ply_io import *
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 def read(path, refl_field='intensity', refl_filter=0.):
 
     """
@@ -61,34 +63,34 @@ def find(pc, sticker_size=.025, W=50, rgb=False, verbose=False):
         number
     """
 
+
     W = 50
     pc.loc[:, 'xx'] = (pc.x // W) * W
     pc.loc[:, 'yy'] = (pc.y // W) * W
-        
     label_max = 0
     pc.loc[:, 'sticker_labels_'] = -1
     
     for tx, ty in pc.groupby(['xx', 'yy']).size().index:
-        
+       
         sq = pc[(pc.xx == tx) & (pc.yy == ty)]
-        if verbose: print 'processing grid {} {} with length {}'.format(tx, ty, len(sq))
-               
+        if verbose: print('processing grid {} {} with length {}'.format(tx, ty, len(sq)))
+              
         dbscan = DBSCAN(eps=sticker_size * 1.1, min_samples=2).fit(sq[['x', 'y', 'z']])
         idx = np.where(dbscan.labels_ > -1)[0]
         pc.loc[sq.loc[sq.index[idx]].index, 'sticker_labels_'] = dbscan.labels_[idx] + label_max
         label_max = pc.sticker_labels_.max() + 1 # iterative labelling hence 
-            
+
     pc = pc[pc.sticker_labels_ != -1] # remove outlier points
-            
+           
     # points can be coloured by cluster for visualisation
     if rgb:
         RGB = pd.DataFrame({l:np.random.randint(0, high=255, size=3) for l in pc.sticker_labels_.unique()}).T
         RGB.columns = ['red', 'green', 'blue']
         pc = pd.merge(pc, RGB, left_on=pc.sticker_labels_, right_on=RGB.index.values, how='outer')
-    
+   
     return pc 
 
-def filterBySize(pc, max_size=.05, verbose=False):
+def filterBySize(pc, max_size=.05, min_size=0, verbose=False):
 
     """
     removes potential stickers that are too big e.g. reflective targets
@@ -114,14 +116,14 @@ def filterBySize(pc, max_size=.05, verbose=False):
     potential_dots = pc.groupby('sticker_labels_').agg({'x':(np.ptp, np.mean), 'y':(np.ptp, np.mean), 'z':(np.ptp, np.mean)})
     potential_dots.columns = ['y_ptp', 'y', 'x_ptp', 'x', 'z_ptp', 'z']
     N = len(potential_dots)
-    if verbose: print "potential stickers found:", N 
+    if verbose: print("potential stickers found:", N) 
     
     # filter by size
     func = lambda row: np.max([row['x_ptp'], row['y_ptp'], row['z_ptp']])
     potential_dots.loc[:, 'max_ptp'] = potential_dots.apply(func, axis=1)
-    potential_dots = potential_dots[potential_dots.max_ptp < max_size]
-    if verbose: print 'stickers removed for being too large', N - len(potential_dots) 
-    if verbose: print 'number of potential stickers:', len(potential_dots)
+    potential_dots = potential_dots[potential_dots.max_ptp.between(min_size, max_size)]
+    if verbose: print('stickers removed for being too large', N - len(potential_dots)) 
+    if verbose: print('number of potential stickers:', len(potential_dots))
 
     return pc[pc.sticker_labels_.isin(potential_dots.index)] 
 
