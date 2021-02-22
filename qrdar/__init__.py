a90c9d256640c4982c76057d0792e87475a2936c
philwilkes <p.wilkes@ucl.ac.uk>
2021-02-22 09:42:07 +0000

    added more import statements

--- qrdar/__init__.py
+++ qrdar/__init__.py
@@ -1,5 +1,7 @@
+from qrdar.io import *
 from .search4stickers import read
 from . import search4stickers
 from .locateTargets import *
 from . import readMarker
 from .extractFeatures import extractFeatures
+from .scripts.identify_codes import identify_codes_in_pc as identify_codes
